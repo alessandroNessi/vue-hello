e78c5a86232fe006cccfb3aea3aeda98a077bf2d
alessandroNessi <alessandronessi@msn.com>
2021-09-16 18:03:08 +0200

    added some if

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const sales={duck:10,house:20,dog:0};
 var app = new Vue({
     el: '#app',
     data: {
@@ -7,12 +8,15 @@ var app = new Vue({
         },
         products:{
             duck:{
-                name: 'rubber duck',
+                name: 'duck',
                 image: {
                     description:"kwaak",
                     url:"http://clipart-library.com/data_images/6103.png",
                     class: "productImage"
                 },
+                stock: 10,
+                // sale:sales.(this.name),
+                sale:10,
                 link: "https://www.google.com/search?q=rubberduck&sxsrf=AOaemvKwQyFlCBbnhvbPYP-HEgj11R4ufg:1631805936775&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjvmNDp5oPzAhXCyaQKHdD5BX0Q_AUoAXoECAEQAw&biw=1920&bih=937&dpr=1"
             }
         }
