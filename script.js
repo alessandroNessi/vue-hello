d5294dfebcc810096df2e337c1dc23a456e3aee1
alessandroNessi <alessandronessi@msn.com>
2021-09-16 16:40:52 +0200

    added a title

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 var app = new Vue({
     el: '#app',
     data: {
+        page: {
+            title: "title"
+        },
         message: 'hello world',
-        // cippa:"http://clipart-library.com/data_images/6103.png"
+        title: `Welcome to booleaner' shop`,
         image: {
             description:"kwaak",
             url:"http://clipart-library.com/data_images/6103.png"
         }
-    },
-    methods: {
-        reverseMessage: function () {
-            this.message = this.message.split('').reverse().join('')
-        }
     }
-})
+    // methods: {
+    //     reverseMessage: function () {
+    //         this.message = this.message.split('').reverse().join('')
+    //     }
+    // }
+});
