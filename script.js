cd9d8d66e8b5ab91c8945cf3553932ce3463e0cf
alessandroNessi <alessandronessi@msn.com>
2021-09-16 15:52:23 +0200

    risolto

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'hello world',
-        url="http://clipart-library.com/data_images/6103.png"
-        // image: {
-        //     description:"cippalippa",
-        //     url="http://clipart-library.com/data_images/6103.png"
-        // }
+        // cippa:"http://clipart-library.com/data_images/6103.png"
+        image: {
+            description:"kwaak",
+            url:"http://clipart-library.com/data_images/6103.png"
+        }
     },
     methods: {
         reverseMessage: function () {
