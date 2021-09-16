417046ddc88a21bc9959f6b0856e07d396685232
alessandroNessi <alessandronessi@msn.com>
2021-09-16 15:41:01 +0200

    errore

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'hello world',
-        image: {
-            description:"cippalippa",
-            url="http://clipart-library.com/data_images/6103.png"
-        }
+        url="http://clipart-library.com/data_images/6103.png"
+        // image: {
+        //     description:"cippalippa",
+        //     url="http://clipart-library.com/data_images/6103.png"
+        // }
     },
     methods: {
         reverseMessage: function () {
