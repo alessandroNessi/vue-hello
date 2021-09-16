501e0a2d154ce447142d5e43ff1462639eb7208e
alessandroNessi <alessandronessi@msn.com>
2021-09-16 17:26:27 +0200

    added some things

--- script.js
+++ script.js
@@ -2,13 +2,19 @@ var app = new Vue({
     el: '#app',
     data: {
         page: {
-            title: "title"
+            title: "title",
+            titleText: `Welcome to booleaner' shop`
         },
-        message: 'hello world',
-        title: `Welcome to booleaner' shop`,
-        image: {
-            description:"kwaak",
-            url:"http://clipart-library.com/data_images/6103.png"
+        products:{
+            duck:{
+                name: 'rubber duck',
+                image: {
+                    description:"kwaak",
+                    url:"http://clipart-library.com/data_images/6103.png",
+                    class: "productImage"
+                },
+                link: "https://www.google.com/search?q=rubberduck&sxsrf=AOaemvKwQyFlCBbnhvbPYP-HEgj11R4ufg:1631805936775&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjvmNDp5oPzAhXCyaQKHdD5BX0Q_AUoAXoECAEQAw&biw=1920&bih=937&dpr=1"
+            }
         }
     }
     // methods: {
